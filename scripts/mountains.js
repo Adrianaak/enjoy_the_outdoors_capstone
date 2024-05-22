4de34f1be658ffc7fa948e15fe2048b60c5084d2
Adriana Kendricks <kendricksadriana16@gmail.com>
2024-05-22 15:28:35 -0500

    updated mountains.js file

--- scripts/mountains.js
+++ scripts/mountains.js
@@ -1,66 +1,91 @@
-"use strict"
-
-
-
+"use strict";
 
 window.onload = () => {
-
+    // Run the code that populates the mountain dropdown
     initMountainDropdown();
 
-    let mountainDropdown = document.querySelector("#mountainSelect")
-
-    //we want to display selected pet when the dropdown is changed
-    mountainDropdown.addEventListener("change", displayMountainCard)
-
-
-}
-
-
-function displayMountainCard(event) {
-
-    let cardImage = document.querySelector("#cardImage");
-    cardImage.src = mountain.image;
-    cardImage.alt = mountain.name;
-
-    let cardTitle = document.querySelector("#cardTitle");
-    cardTitle.innerHTML = mountain.name
-
-}
-
-
-
-function initMountainDropdown(mountain) {
-
-    //let's get a hold of the dropdown for the categories from the activities.html
+    // Wrap the event listener registration inside the window.onload function
+    // to ensure that the DOM is fully loaded before accessing its elements
     let mountainDropdown = document.querySelector("#mountainSelect");
 
-    //create the element for the default option
+    if (mountainDropdown) {
+        // Make sure the element exists before trying to add an event listener
+        mountainDropdown.addEventListener("change", getMountains);
+    }
+}
+
+function getMountains(event) {
+    // Get the selected mountain from the dropdown
+    let selectedMountain = event.target.value;
+
+    // Find the mountain data
+    let mountain = mountainsArray.find(mountain => mountain.name === selectedMountain);
+
+    // Get a hold of the card body
+    let cardBody = document.querySelector("#cardBody");
+
+    // Clear the innerHTML
+    cardBody.innerHTML = "";
+
+    // Build the card with mountain data
+    buildCard(cardBody, mountain);
+}
+
+function buildCard(cardBody, data) {
+    // Create the card element
+    let card = document.createElement("div");
+    card.classList.add("card");
+
+    // Create card body
+    let cardContent = document.createElement("div");
+    cardContent.classList.add("card-content");
+
+    // Create card title
+    let cardTitle = document.createElement("span");
+    cardTitle.classList.add("card-title");
+    cardTitle.textContent = data.name;
+
+    // Create card image
+    let cardImage = document.createElement("img");
+    cardImage.classList.add("card-img-top");
+   // cardImage.classList.add("w-50")
+    cardImage.src = "../enjoy_the_outdoors_capstone/images/" + data.img;
+
+    cardImage.alt = data.name;
+
+    console.log(data.img)
+
+    // Create card description
+    let cardDescription = document.createElement("p");
+    cardDescription.textContent = data.desc;
+
+    // Append elements to card
+    cardContent.appendChild(cardImage);
+    cardContent.appendChild(cardTitle);
+    cardContent.appendChild(cardDescription);
+    card.appendChild(cardContent);
+
+    // Append card to card body
+    cardBody.appendChild(card);
+}
+
+function initMountainDropdown() {
+    // Get the dropdown for the mountains
+    let mountainsDropdown = document.querySelector("#mountainSelect");
+
+    // Create the default option
     let defaultOption = document.createElement("option");
-
-    //this is whate we get back in JS when we ask for the selected value
     defaultOption.value = "";
+    defaultOption.textContent = "";
 
-    //this is what the user actually selects from the dropdown
-    defaultOption.textContent = "Select a Mountain";
+    // Append default option
+    mountainsDropdown.appendChild(defaultOption);
 
-    //add the option we created to the dropdown
-    mountainDropdown.appendChild(defaultOption);
-
-    //write a loop to work with each individual category and build an option for it
-    mountain.forEach((m) => {
-
-        //create the new option for the category we are on in the loop
-        let newOption = document.createElement("option");
-
-        //set the value for the option
-        newOption.value = m.name;
-
-        //set the textContent that the user will see when choosing a category
-        newOption.textContent = m.name;
-
-        mountainDropdown.appendChild(newOption);
-
-    })
-
-
+    // Loop through mountainsArray and create options
+    mountainsArray.forEach(mountain => {
+        let option = document.createElement("option");
+        option.value = mountain.name;
+        option.textContent = mountain.name;
+        mountainsDropdown.appendChild(option);
+    });
 }
