d9cbce9b4bd34be5a804425998545a2987a182eb
Adriana Kendricks <kendricksadriana16@gmail.com>
2024-05-23 15:08:07 -0500

    Updated dropdowns and added more functions to Files

--- scripts/national_parks.js
+++ scripts/national_parks.js
@@ -4,14 +4,55 @@ window.onload = () => {
     // Run the code that populates the category dropdown
     initCategoriesDropdown();
 
+    initParkTypeSelect();
+
     // Wrap the event listener registration inside the window.onload function
     // to ensure that the DOM is fully loaded before accessing its elements
     let categoriesDropdown = document.querySelector("#nationalParkSelect");
+    let parkTypeDropdown = document.querySelector("#parkTypeSelect");
 
     if (categoriesDropdown) {
         // Make sure the element exists before trying to add an event listener
         categoriesDropdown.addEventListener("change", getNationalParks);
+
     }
+    parkTypeDropdown.addEventListener("change", getParksByType);
+
+    let locationRadio = document.querySelector("#locationRadio");
+    let typeRadio = document.querySelector("#typeRadio");
+
+    hideElement("#nationalParkSelect");
+    hideElement("#parkTypeSelect");
+
+    locationRadio.addEventListener("change",hideShowDropdown)
+    
+    typeRadio.addEventListener("change", hideShowDropdown)
+}
+
+function hideShowDropdown(event) {
+
+    if (event.target.value === "location") {
+        showElement("#nationalParkSelect");
+        hideElement("#parkTypeSelect");
+    } else {
+        showElement("#parkTypeSelect");
+        hideElement("#nationalParkSelect");
+    }
+
+}
+
+//This function will hide an HTML element on the page
+//Just pass it the id of the element you want to hide
+function hideElement(someSelector) {
+    let el = document.querySelector(someSelector);
+    el.style.display = "none";
+}
+
+//This function will show an HTML element on the page
+//Just pass it the id of the element you want to show
+function showElement(someSelector) {
+    let el = document.querySelector(someSelector);
+    el.style.display = "block";
 }
 
 function getNationalParks(event) {
@@ -54,7 +95,7 @@ function initCategoriesDropdown() {
     // Create the default option
     let defaultOption = document.createElement("option");
     defaultOption.value = "";
-    defaultOption.textContent = "Select a Park";
+    defaultOption.textContent = "Select a Location";
 
     // Append default option
     categoriesDropdown.appendChild(defaultOption);
@@ -81,4 +122,47 @@ function getParkInCategory(park, category) {
 
     // Return all the matching menu items
     return matching;
+}
+
+function initParkTypeSelect() {
+    // Get the dropdown for the categories
+    let parkTypeDropdown = document.querySelector("#parkTypeSelect");
+
+    // Create the default option
+    let defaultOption = document.createElement("option");
+    defaultOption.value = "";
+    defaultOption.textContent = "Select a Park Type";
+
+    // Append default option
+    parkTypeDropdown.appendChild(defaultOption);
+
+    // Loop through locationsArray and create options
+    parkTypesArray.forEach((type) => {
+        let newOption = document.createElement("option");
+        newOption.value = type;
+        newOption.textContent = type;
+        parkTypeDropdown.appendChild(newOption);
+    });
+}
+
+function getParksByType(event) {
+    // Get the selected National Park from the dropdown which is also the event.target
+    let selectedNationalPark = event.target.value;
+    console.log(selectedNationalPark)
+
+    // Find the matching parks
+    let matchingNationalParks = nationalParksArray.filter((park) => {
+        return park.LocationName.indexOf(selectedNationalPark) !== -1;
+    });
+
+    // Get a hold of the table body
+    let tableBody = document.querySelector("#nationalParkTableBody");
+
+    // Clear the innerHTML
+    tableBody.innerHTML = "";
+
+    // Loop through matching parks and build table rows
+    matchingNationalParks.forEach((park) => {
+        buildTableRow(tableBody, park);
+    });
 }
